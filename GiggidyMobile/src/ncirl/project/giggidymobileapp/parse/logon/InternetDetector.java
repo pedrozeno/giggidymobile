f7c3d8b7474d0b2941ef02d31784d58294e99577
Paddy Byrne <paddyb81@gmail.com>
2014-11-05 22:17:34 +0000

    Parse classes added

---
+++ GiggidyMobile/src/ncirl/project/giggidymobileapp/parse/logon/InternetDetector.java
@@ -0,0 +1,35 @@
+package ncirl.project.giggidymobileapp.parse.logon;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class InternetDetector {
+	
+	private Context context;
+	
+	public InternetDetector(Context context){
+		this.context = context;
+		
+	}
+	
+	public boolean isConnectedToInternet(){
+		
+		ConnectivityManager connector = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		
+		if(connector !=null){
+			
+			NetworkInfo[] netInfo = connector.getAllNetworkInfo();
+			  if (netInfo != null) 
+				  for (int i = 0; i < netInfo.length; i++) 
+					  if (netInfo[i].getState() == NetworkInfo.State.CONNECTED)
+					  {
+						  return true;
+					  }
+
+		  }
+		
+		return false;
+	}
+
+}
